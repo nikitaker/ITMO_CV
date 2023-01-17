013d98340e5631418f3dd41d3830d53ac20d9ff9
Керенский Никита Александрович <NAKerenskiy@sberbank.ru>
2023-01-17 15:45:16 +0300

    lab 1 work

--- Lab 2/lab2_template.py
+++ Lab 2/lab2_template.py
@@ -5,27 +5,17 @@ source = cv2.imread("source.png")
 template = cv2.imread("another.png")
 (templateHeight, templateWidth) = template.shape[:2]
 
-# ищем template на основном изображении,
 result = cv2.matchTemplate(source, template, cv2.TM_SQDIFF)
-
-# ищем минимум в хитмапе
+# If you are using cv.TM_SQDIFF as comparison method, minimum value gives the best match.
 (_, _, minLoc, maxLoc) = cv2.minMaxLoc(result)
-
 
 topLeft = minLoc
 botRight = (topLeft[0] + templateWidth, topLeft[1] + templateHeight)
-roi = source[topLeft[1]:botRight[1], topLeft[0]:botRight[0]]
 
-mask = np.zeros(source.shape, dtype="uint8")
-
-new_image = cv2.addWeighted(source, 0.5, mask, 1, 0)
-
-
-# выделяем нужную нам область на изображении
-new_image[topLeft[1]:botRight[1], topLeft[0]:botRight[0]] = roi
+cv2.rectangle(source, topLeft, botRight, 255, 2)
 
 # display the images
-cv2.imshow("Main_image", new_image)
+cv2.imshow("Main_image", source)
 cv2.imshow("Template", template)
-cv2.imwrite("template_matched.png", new_image)
+cv2.imwrite("template_matched.png", source)
 cv2.waitKey(0)
